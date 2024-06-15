c1fec4497d63f9be75487c3fe49d1782bec8ecc9
Chappie733 <41633580+Chappie733@users.noreply.github.com>
2024-06-15 15:20:55 +0200

    Update sforzo.py

--- sforzo.py
+++ sforzo.py
@@ -1,28 +1,65 @@
 import numpy as np
 
-def calc_sigma(massa=0.001):
+
+# theta_0 in gradi, mass in kg, w in rpm
+def calc_gittata(theta_0, mass, w = 3400, dt=1e-6):
     M_lama = 0.5
-    R_sasso = (3*massa / (4*np.pi*2400))**(1/3)          # calcolo del raggio per una sfera perfetta assumendo densità 2400 Kg/m^3
-    v_lama = 134
-    v_sasso = v_lama * 2*M_lama / (M_lama+massa)
+    r = 0.377
+    w *= 2*np.pi/60     # converti
+    v_lama = w*r
 
-    dt = 2*R_sasso/v_lama
-    Js = massa*v_sasso
-    F_m = Js/dt
-    S_max = np.pi*((R_sasso)**2)
+    theta_0 = theta_0 * np.pi/180
+    h_0 = 0.05
+    pos = np.array([0, h_0], dtype=np.float64)
+    v_0 = 2*M_lama/(M_lama+mass) * v_lama
+    vel = np.array([np.cos(theta_0), np.sin(theta_0)], dtype=np.float64)*v_0
+    
+    R_sasso = (3*mass / (4*np.pi*2400))**(1/3)          # calcolo del raggio per una sfera perfetta assumendo densità 2400 Kg/m^3
+    S = np.pi*((R_sasso)**2)                            # area trasversale
+    mu = 1.225*S*0.59/2    # sfera perfetta
+    
+    
+    g = 9.81
+    t = 0
+    counter = 0
+    m = 0.001
 
-    sigma = F_m/S_max
-    return sigma
+    past_positions = []
+    past_velocities = []
+    print("Inizio calcolo")
 
+    # finché il corpo non ha toccato terra
+    while pos[1] > R_sasso:
+        v_mag = np.linalg.norm(vel)         # modulo dell'accelerazione
+        a = -mu*v_mag*vel / m               # calcolo dell'accelerazione con i termini in comune ai due componenti
+        a[1] += -g                          # aggiunta accelerazione per gravità
 
-masses = np.linspace(0.001, 0.1, 50)
-sforzi = []
-for val in masses:
-    sforzo = calc_sigma(val)
-    sforzi.append(sforzo*1e-6)
+        new_pos = pos + dt*vel              # x_{n+1} = x_n + dt * v_n
+        new_vel = vel + dt*a                # v_{n+1} = v_n + dt * a_n
+        t += dt
+
+        #past_positions.append((t, new_pos))     # per fare grafici
+        #past_velocities.append((t, new_vel))
+
+        vel = new_vel
+        pos = new_pos
+
+        counter += 1
+        if counter >= 0.5/dt: # ogni 0.5 secondi nella simulazione simulazione aggiorna sullo stato
+            counter = 0
+            print(f"t:   {t},   y:   {pos[1]},   vel:   {np.linalg.norm(vel)},   x: {pos[0]}")
+
+    return pos[0]
+
+n = 5
+vals = []
+degs = np.linspace(31/n, 31, n)
+for deg in degs:
+    x_max = calc_gittata(deg, 0.01, dt=1e-6)
+    vals.append(x_max)
 
 import matplotlib.pyplot as plt
-plt.xlabel('massa (Kg)')
-plt.ylabel('sforzo (MPa)')
-plt.plot(masses, sforzi)
+plt.xlabel('Angolo di proiezione (gradi)')
+plt.ylabel('Gittata massima (m)')
+plt.plot(degs, vals)
 plt.show()
